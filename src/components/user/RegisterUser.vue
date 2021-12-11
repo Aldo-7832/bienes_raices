764eeccc1be70d41d6883757f2880a327239c950
NoeliaGBP <20193tn024@utez.edu.mx>
2021-12-11 15:16:45 -0600

    Actualización de componente RegisterUser y view Pricing

--- src/components/user/RegisterUser.vue
+++ src/components/user/RegisterUser.vue
@@ -3,48 +3,57 @@
         <div class="text-center">
             <img src="../../assets/user.png" alt="user" height="150">
         </div>
-        <div class="row mt-4">
-            <div class="col-4">
-                <label class="form-label">Nombre(s)</label>  
-                <input type="text" class="form-control" placeholder="Nombre(s)" required>
+        <form class="row g-3 needs-validation" validate>
+            <div class="row mt-4">
+                <div class="col-4">
+                    <label for="name" class="form-label">Nombre(s)</label>  
+                    <input id="name" type="text" class="form-control" 
+                    value="" placeholder="Nombre(s)" required>
+                </div>
+                <div class="col-4">
+                    <label for="lastName" class="form-label">Primer apellido</label>  
+                    <input id="lastName" type="text" class="form-control" 
+                    value="" placeholder="Primer apellido" required>
+                </div>
+                <div class="col-4">
+                    <label for="lastSecondName" class="form-label">Segundo apellido</label>  
+                    <input id="lastSecondName" type="text" class="form-control" 
+                    value="" placeholder="Segundo apellido">
+                </div>
             </div>
-            <div class="col-4">
-                <label class="form-label">Primer apellido</label>  
-                <input type="text" class="form-control" placeholder="Primer apellido" required>
+            <div class="row mt-4">
+                <div class="col-4">
+                    <label for="email" class="form-label">Correo electrónico</label>  
+                    <input id="email" type="text" class="form-control" 
+                    value="" placeholder="Correo electrónico" required>
+                </div>
+                <div class="col-4">
+                    <label for="password" class="form-label">Contraseña</label>  
+                    <input id="password" type="password" class="form-control" 
+                    value="" placeholder="Contraseña" required>
+                </div>
+                <div class="col-4">
+                    <label for="passwordConfirmation" class="form-label">Confirmación de contraseña</label>  
+                    <input id="passwordConfirmation" type="password" class="form-control" 
+                    value="" placeholder="Contraseña" required>
+                </div>
             </div>
-            <div class="col-4">
-                <label class="form-label">Segundo apellido</label>  
-                <input type="text" class="form-control" placeholder="Segundo apellido" required>
+            <div class="d-flex justify-content-end mt-4">
+                <button type="button" v-on:click="register" class="btn btn-primary">Registrar</button>
             </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-4">
-                <label class="form-label">Correo electrónico</label>  
-                <input type="text" class="form-control"placeholder="Correo electrónico" required>
-            </div>
-            <div class="col-4">
-                <label class="form-label">Contraseña</label>  
-                <input type="password" class="form-control" placeholder="Contraseña" required>
-            </div>
-            <div class="col-4">
-                <label class="form-label">Confirmación de contraseña</label>  
-                <input type="password" class="form-control" placeholder="Contraseña" required>
-            </div>
-        </div>
-        <div class="d-flex justify-content-end mt-4">
-            <button type="submit" class="btn btn-primary">Registrar</button>
-        </div>
+        </form>
     </div>
 </template>
 
 <script>
 export default {
   name: "RegisterUser",
-  data() {
-    return {};
+  methods: {
+    register() {
+      this.$router.push("/login");
+    },
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,15 +1,78 @@
-<template>
-  <div>
- <h1>Pricing</h1>
+<template lang="es">
+<div>
+  <div class="mx-5">
+    <h1>Obtén beneficios según tu nivel</h1>
+    <hr class="line" >
+    <div class="row text-center d-flex justify-content-center">
+      <div class="cardInit col-3">
+        <div class=" border bg-secondary border-secondary card my-3 bg-opacity-50">
+          <h3 class="p-3 pb-5">Free</h3>
+          <p>Fecha de caducidad: 10 días</p>
+          <p>Propiedades: 10</p>
+          <p>Listas destacadas: 5</p>
+          <div class="card bg-white mb-3">
+            <p class="fs-1 p-3">$129</p>
+          </div>
+        </div>
       </div>
+      <div class="cardInit col-3">
+        <div class=" border border-secondary card  my-3">
+          <h3 class="p-3 pb-5">Standard</h3>
+          <p>Fecha de caducidad: 3 meses</p>
+          <p>Propiedades: 20</p>
+          <p>Listas destacadas: 10</p>
+          <div class="card bg-secondary bg-opacity-50 mb-3">
+            <p class="fs-1 p-3">$159</p>
+          </div>
+        </div>
+      </div>
+      <div class="cardInit col-3">
+        <div class=" border bg-secondary border-secondary card  my-3 bg-opacity-50">
+          <h3 class="p-3 pb-5">Premium</h3>
+          <p>Fecha de caducidad: 1 año</p>
+          <p>Propiedades: 100</p>
+          <p>Listas destacadas: 50</p>
+          <div class="card bg-white mb-3">
+            <p class="fs-1 p-3">$179</p>
+          </div>
+          </div>
+      </div>
+      <div class="cardInit col-3">
+        <div class="border border-secondary card  my-3">
+          <h3 class="p-3 pb-5">Gold</h3> <p>Fecha de caducidad: 2 años</p>
+          <p>Propiedades: Ilimitado</p>
+          <p>Listas destacadas: 50</p>
+          <div class="card bg-secondary bg-opacity-50 mb-3">
+            <p class="fs-1 p-3">$199</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Footer />
+</div>
 </template>
 
 <script>
+import Footer from "../components/Footer";
+
 export default {
-    name: "Pricing"
-}
+  name: "Pricing",
+  components: { Footer },
+};
 </script>
 
-<style scoped>
-
+<style>
+.line {
+  border-width: 2px;
+  border-style: solid;
+  color: black;
+}
+.card {
+  border-radius: 25px;
+  margin: 20px;
+}
+.cardInit {
+  padding: 5px;
+}
 </style>
