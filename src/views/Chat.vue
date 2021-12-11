75933fdecd44d2f377f7a86f414d7b5a9f90356c
unknown <20193tn013@utez.edu.mx>
2021-12-11 14:32:53 -0600

    Chat.vue & Mailbox.vue

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,15 +1,121 @@
 <template>
   <div>
-        <h1>Chat</h1>
+    <div class="col-lg-12">
+      <div class="card shadow-sm-mt-2">
+        <div class="card-header">
+          <div class="col-lg-12 d-flex">
+            <div><button class="btn-back"><b>&lt;</b></button></div>
+            <div style="margin-right: 10px"><img src="../assets/user.png"></div>
+            <div class="mt-2"><b>Nombre</b></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-3">
+      <div class="col-lg-12">
+      <input type="text" class="chat1" value="Mensaje 1" readonly />
+    </div>
+    <div class="col-lg-12 text-end">
+      <input type="text" class="chat2" value="Mensaje 2" readonly />
+    </div>
+    <div class="col-lg-12">
+      <input type="text" class="chat1" value="Mensaje 3" readonly />
+    </div>
+    <div class="col-lg-12 text-end">
+      <input type="text" class="chat2" value="Mensaje 4" readonly />
+    </div>
+    <div class="col-lg-12">
+      <input type="text" class="chat1" value="Mensaje 5" readonly />
+    </div>
+    <div class="col-lg-12 text-end">
+      <input type="text" class="chat2" value="Mensaje 6" readonly />
+    </div>
+    </div>
+    <div class="bottom-navbar">
+      <div class="card-footer">
+        <div class="col-lg-12 d-flex">
+          <button class="btn-extra"><b>+</b></button>
+          <input type="text" class="message-box" placeholder="Mensaje" />
+          <button class="btn-back" style="margin-left: 10px"><b>&gt;</b></button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Chat"
-}
+  name: "Chat",
+};
 </script>
 
 <style scoped>
-
+.header {
+  width: 100% !important;
+  background-color: #25d87c;
+  height: 45px;
+}
+.message-box {
+  width: 10000px;
+  height: 40px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border: 1px solid rgb(204, 204, 204);
+  border-radius: 14px;
+}
+.chat1 {
+  width: 400px;
+  height: 40px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 15px;
+  border: 1px solid #1daa61;
+  border-radius: 14px;
+  outline: none;
+  background-color: #7febb3;
+}
+.chat2 {
+  width: 400px;
+  height: 40px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-right: 15px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border: 1px solid #1daa61;
+  border-radius: 14px;
+  outline: none;
+  background-color: #25e280;
+}
+.btn-extra {
+  width: 40px;
+  border-radius: 20px;
+  outline: none;
+  border: 1px solid rgb(219, 219, 219);
+  margin-right: 10px;
+  background-color: #fff;
+}
+.btn-back {
+  width: 40px;
+  border-radius: 20px;
+  height: 40px;
+  outline: none;
+  border: 1px solid rgb(219, 219, 219);
+  margin-right: 10px;
+  background-color: #fff;
+}
+img
+{
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+}
+.top-navbar,
+.bottom-navbar {
+  position: fixed;
+  width: 100%;
+}
+.bottom-navbar {
+  bottom: 0;
+}
 </style>
--- src/views/Mailbox.vue
+++ src/views/Mailbox.vue
@@ -1,15 +1,159 @@
 <template>
   <div>
- <h1>Mailbox</h1>
+    <div class="wrapper">
+      <div class="sidebar">
+        <img src="../assets/user.png">
+        <h2>Nombre</h2>
+        <ul>
+          <li><a href="#">Redactar</a></li>
+          <li><a href="#">Recibidos</a></li>
+          <li><a href="#">Enviados</a></li>
+          <li><a href="#">Spam</a></li>
+        </ul>
       </div>
+      <div class="main_content">
+        <div class="header"><input type="text" placeholder="Buscar"></div>
+        <div class="info">
+          <div class="mail">
+            <div class="persona">Persona</div>
+            <div class="asunto">Asunto</div>
+            <div class="borrar"><p style="text-align:right">Borrar</p></div>
+          </div>
+          <div class="mail">
+            <div class="persona">Persona</div>
+            <div class="asunto">Asunto</div>
+            <div class="borrar"><p style="text-align:right">Borrar</p></div>
+          </div>
+          <div class="mail">
+            <div class="persona">Persona</div>
+            <div class="asunto">Asunto</div>
+            <div class="borrar"><p style="text-align:right">Borrar</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Mailbox"
-}
+  name: "Mailbox",
+};
 </script>
 
 <style scoped>
+*
+{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+}
+.wrapper
+{
+  display: flex;
+  position: relative;
 
+}
+.wrapper .sidebar
+{
+  position: fixed;
+  width: 200px;
+  height: 100%;
+  background: #25D87C;
+  padding:30px 0;
+}
+.wrapper .sidebar h2
+{
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.wrapper .sidebar ul li
+{
+  padding: 15px;
+  border-bottom: 1px solid #28D825;
+  border-top: 1px solid #28D825;
+}
+.wrapper .sidebar ul li a
+{
+  color: #002411;
+  display: block;
+}
+.wrapper .sidebar ul li a .fas
+{
+  width: 25px;
+}
+.wrapper .sidebar ul li:hover
+{
+  background: #00e783;
+}
+.wrapper .sidebar ul li:hover a
+{
+  color: #025c2c;
+}
+.wrapper .main_content
+{
+  width: 100%;
+  margin-left: 200px;
+}
+.wrapper .main_content .header
+{
+  padding: 20px;
+  background: #fff;
+  color: #717171;
+  border-bottom: 1px solid #025c2c;
+}
+.wrapper .main_content .header input
+{
+  height: 40px;
+  width: 100%;
+  border-radius: 10px;
+  border: 1px solid;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.wrapper .main_content .info
+{
+  margin: 20px;
+}
+.wrapper .main_content .info .mail
+{
+  display: flex;
+  background: #e9e9e9;
+  border-bottom: 1px solid #28D825;
+  border-top: 1px solid #28D825;
+}
+.wrapper .main_content .info .mail .persona
+{
+  font-size: 20px;
+  margin-left: 5px;
+}
+.wrapper .main_content .info .mail .asunto
+{
+  width: 200px;
+  font-size: 20px;
+  margin-left: 50px;
+}
+.wrapper .main_content .info .mail .borrar
+{
+  font-size: 20px;
+  width:80%;
+  text-align:right;
+  margin-right: 10px;
+}
+.wrapper .main_content .info .mail:hover
+{
+  background: #c0c0c0;
+}
+.wrapper .sidebar img
+{
+  height: 100px;
+  width: 100px;
+  border-radius: 100px;
+  margin-left: 25%;
+  margin-bottom: 10px;
+}
 </style>
